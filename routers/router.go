b7c14bc8c67dc273d9af39b6845276cecc8da941
kstwoak <kstwoak47@163.com>
2018-05-27 12:56:14 +0800

    添加错误码

--- routers/router.go
+++ routers/router.go
@@ -7,4 +7,16 @@ import (
 
 func init() {
     beego.Router("/", &controllers.MainController{})
+    //请求地域信息
+	beego.Router("/api/v1.0/areas", &controllers.AreaController{},"get:GetAreaInfo")
+
+	//session请求
+	beego.Router("/api/v1.0/session", &controllers.SessionController{},"get:GetSessionInfo")
+
+	//index 请求
+	beego.Router("/api/v1.0/houses/index ", &controllers.HousesIndexController{},"get:GetHousesIndexInfo")
+	//user请求
+	beego.Router("/api/v1.0/users", &controllers.UserController{},"post:PostUserInfo")
+
+
 }

a4ad87ee581ddcdeb7770876699f56a340d39c6f
kstwoak <kstwoak47@163.com>
2018-05-30 12:03:59 +0800

    areas session sessions avater index users 6个基本功能

--- routers/router.go
+++ routers/router.go
@@ -11,12 +11,19 @@ func init() {
 	beego.Router("/api/v1.0/areas", &controllers.AreaController{},"get:GetAreaInfo")
 
 	//session请求
-	beego.Router("/api/v1.0/session", &controllers.SessionController{},"get:GetSessionInfo")
+	beego.Router("/api/v1.0/session", &controllers.SessionController{},"get:GetSessionInfo;delete:DelSessionInfo")
+
+	//sessions 登陆 请求
+	beego.Router("/api/v1.0/sessions", &controllers.UserController{},"post:Login")
+
+	//文件上传业务v1.0/user/avatar
+	beego.Router("/api/v1.0/user/avatar", &controllers.UserController{},"post:UploadAvatar")
+
 
 	//index 请求
 	beego.Router("/api/v1.0/houses/index ", &controllers.HousesIndexController{},"get:GetHousesIndexInfo")
 	//user请求
-	beego.Router("/api/v1.0/users", &controllers.UserController{},"post:PostUserInfo")
+	beego.Router("/api/v1.0/users", &controllers.UserController{}, "post:Reg")
 
 
 }

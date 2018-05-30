54aaae172925e315851bd3102770f132fd39db92
kstwoak <kstwoak47@163.com>
2018-05-30 13:55:44 +0800

    新增fastdfs配套文件

---
+++ fastdfs/storage_data/data/00/00/wKhuB1sNBDGAA9a1AAAC_fnZXKQ1098.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "ilhome/routers"
+	_ "ilhome/models"
+	"strings"
+	"net/http"
+)
+
+func main() {
+	ignoreStaticPath()
+	beego.Run()
+}
+
+func ignoreStaticPath() {
+	//透明static
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+}
+
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+	beego.Debug("request url: ", orpath)
+	//如果请求uri还有api字段,说明是指令应该取消静态资源路径重定向
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+	///index.html --->/static/html/index.html
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
